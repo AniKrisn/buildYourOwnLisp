7ac988fb9d7664784de184dc69ddd4fbb9e1393a
AniKrisn <anikrishnantango@gmail.com>
2024-08-04 19:54:28 +0530

    add mpc files and parsing section ch6

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpc.h>
 
 #ifdef _WIN32
 #include <string.h>
@@ -32,17 +33,49 @@ void add_history(char *unused) {}
 
 int main (int argc, char **argv) {
 
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+
+
     puts("Lispy Version 0.1");
     puts("Press Ctrl+C to escape\n");
 
+
     while (1) {
+
         char *input = readline("lispy> ");
         add_history(input);
         // readline history is stored separately, primarily in the heap. Calling free() doesn't affect this; there are separate functions to read/write the readline history
 
-        printf("No you're a %s!\n", input);
+        mpc_result_t r;
+        // pass user input
+
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+        // if successful, print and delete AST, else error
+
+
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    // undefine and delete the parsers
 
     return 0;
 }
