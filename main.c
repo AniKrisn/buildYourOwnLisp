d49d99340126ecc4f832012494ac4eaa23d41a29
AniKrisn <anikrishnantango@gmail.com>
2024-08-04 07:07:08 +0530

    boilerplate

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <string.h>
+
+
+void add_history(char *unused) {}
+
+
+#else 
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+
 
 int main (int argc, char **argv) {
-    printf("%d\n", argc);
 
-    for (int i=0; i<argc; i++) {
-        printf("%d\n", argv[i]);
+    char* input;
+
+    while (input = readline("lispy> ") != NULL) {
+        add_history(input);
     }
 
 }
